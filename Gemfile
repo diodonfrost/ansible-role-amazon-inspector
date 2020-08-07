6c0cb6e14ee38d0c8e4f256b54fb3cba4886dd9b
diodonfrost <diodon.frost@diodonfrost.me>
2020-08-08 00:37:51 +0200

    removing kitchen-ci pipeline

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source "https://rubygems.org"
-
-gem "test-kitchen", "2.3.1"
-gem "kitchen-ec2", "3.1.0"
-gem "kitchen-ansible", "0.50.1"
-gem "kitchen-inspec", "1.2.0"
-gem "kitchen-sync", "2.2.1"
-gem "inspec-bin", "4.16.0"
--- tests/inspec/inspector_spec.rb
+++
@@ -1,8 +0,0 @@
-control 'inspector-01' do
-  impact 1.0
-  title 'Amazon inspector agent install'
-  desc 'Amazon inspector agent should be installed'
-  describe command('/opt/aws/awsagent/bin/awsagent status') do
-    its('exit_status') { should eq 0 }
-  end
-end
